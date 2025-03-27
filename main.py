035394d676f56d1e90b617b636b97e14dbdee08e
Matheus-Rossetti <matheuzdreher@gmail.com>
2025-03-27 13:12:42 -0300

    removed supabase s3, garbage API

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from flask import Flask, jsonify
 import os
 
-from supabase import create_client, Client
-
-
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
-
-
-supabase: Client = create_client(url, key)
-
 app = Flask(__name__)
 
 
@@ -20,25 +11,15 @@ def hello_world():
 
 @app.route('/video/<video_name>/<file_name>')
 def stream(video_name, file_name):
-    response = (supabase.storage.from_("videos").download(f"{video_name}/{file_name}"))
-    return jsonify(response)
+
+    return jsonify(f'Seu video deveria estar baixando: {video_name} | {file_name}')
 
 @app.route('/lista-de-videos')
 def arquivos_do_balde():
-    response = (
-        supabase.storage
-        .from_("videos")
-        .list(
-            "/",
-            {
-                "limit": 100,
-                "offset": 0,
-                "sortBy": {"column": "name", "order": "desc"},
-            }
-        )
-    )
-    return jsonify(response)
 
+
+
+    return
 
 
 
